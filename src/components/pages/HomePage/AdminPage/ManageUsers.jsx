b31901e8f30dac2c9603357a26a46415c969e8d4
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-07 22:16:31 +0600

    data shown for all the users

--- src/components/pages/HomePage/AdminPage/ManageUsers.jsx
+++ src/components/pages/HomePage/AdminPage/ManageUsers.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable operator-linebreak */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable react/jsx-indent-props */
+import React, { useEffect, useState } from 'react';
 import { RiAdminFill } from 'react-icons/ri';
 
 import { MdEmojiPeople } from 'react-icons/md';
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import SharedTitle from '../../../layouts/shared/SharedTitle';
 
 const ManageUsers = () => {
     const [isClicked, setIclicked] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [axiosSecure] = useAxiosSecure();
+    useEffect(() => {
+        axiosSecure.get('/users').then(({ data }) => setUsers(data.data));
+    }, []);
+    console.log(users);
     const handeleInstrcutor = (id) => {
         setIclicked((ps) => !ps);
     };
@@ -22,27 +33,39 @@ const ManageUsers = () => {
                         <tr>
                             <th>Name</th>
                             <th>Email</th>
+                            <th>Role of the User</th>
                             <th>Make Instructor</th>
                             <th>Make Admin</th>
                         </tr>
                     </thead>
                     <tbody>
                         {/* row 1 */}
-                        <tr>
-                            <td>h1ll</td>
-                            <td>Cy Ganderton</td>
-
-                            <td>
-                                <button type="button" className="btn btn-circle btn-outline" onClick={handeleInstrcutor} disabled={isClicked}>
-                                    <MdEmojiPeople size={30} />
-                                </button>
-                            </td>
-                            <td>
-                                <button type="button" className="btn btn-circle btn-outline" onClick={handeleAdmin} disabled={isClicked}>
-                                    <RiAdminFill size={30} />
-                                </button>
-                            </td>
-                        </tr>
+                        {users &&
+                            users.map((el) => (
+                                <tr key={el?._id}>
+                                    <td>{el?.name}</td>
+                                    <td>{el?.email}</td>
+                                    <td>{el?.role}</td>
+                                    <td>
+                                        <button
+                                            type="button"
+                                            className={`btn btn-circle btn-outline animate-pulse bg-info ${el?.role === 'admin' && 'animate-none'}`}
+                                            onClick={handeleInstrcutor}
+                                            disabled={isClicked || el?.role === 'admin'}>
+                                            <MdEmojiPeople size={30} />
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button
+                                            type="button"
+                                            className={`btn btn-circle btn-outline animate-pulse bg-success ${el?.role === 'admin' && 'animate-none'}`}
+                                            onClick={handeleAdmin}
+                                            disabled={isClicked || el?.role === 'admin'}>
+                                            <RiAdminFill size={30} />
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
                     </tbody>
                 </table>
             </div>

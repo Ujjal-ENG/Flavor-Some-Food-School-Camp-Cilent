67dab3e091072da1e015a7ab95dfba8b7e190258
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-07 23:48:39 +0600

    manage user worked successfully

--- src/components/pages/HomePage/AdminPage/ManageUsers.jsx
+++ src/components/pages/HomePage/AdminPage/ManageUsers.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable comma-dangle */
 /* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable operator-linebreak */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-indent-props */
 import React, { useEffect, useState } from 'react';
-import { RiAdminFill } from 'react-icons/ri';
-
 import { MdEmojiPeople } from 'react-icons/md';
+import { RiAdminFill } from 'react-icons/ri';
+import Swal from 'sweetalert2';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import SharedTitle from '../../../layouts/shared/SharedTitle';
 
@@ -13,15 +15,60 @@ const ManageUsers = () => {
     const [isClicked, setIclicked] = useState(false);
     const [users, setUsers] = useState([]);
     const [axiosSecure] = useAxiosSecure();
+
     useEffect(() => {
         axiosSecure.get('/users').then(({ data }) => setUsers(data.data));
-    }, []);
-    console.log(users);
-    const handeleInstrcutor = (id) => {
-        setIclicked((ps) => !ps);
+    }, [isClicked]);
+
+    const handeleInstrcutor = async (id) => {
+        const result = await Swal.fire({
+            title: 'Are you sure?',
+            text: 'are your sure to make this Instructor?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, make it!'
+        });
+        if (result.isConfirmed) {
+            try {
+                await axiosSecure.patch(`/users/admin/${id}`, { role: 'instructor' });
+                Swal.fire('Updated!', 'The user has been updated to Instructor.', 'success');
+                setIclicked(id);
+            } catch (error) {
+                console.log(error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: error.message
+                });
+            }
+        }
     };
-    const handeleAdmin = (id) => {
-        setIclicked((ps) => !ps);
+    const handeleAdmin = async (id) => {
+        const result = await Swal.fire({
+            title: 'Are you sure?',
+            text: 'are your sure to make this Admin?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, make it!'
+        });
+        if (result.isConfirmed) {
+            try {
+                await axiosSecure.patch(`/users/admin/${id}`, { role: 'admin' });
+                Swal.fire('Updated!', 'The user has been updated to Admin.', 'success');
+                setIclicked(id);
+            } catch (error) {
+                console.log(error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: error.message
+                });
+            }
+        }
     };
     return (
         <div>
@@ -50,8 +97,8 @@ const ManageUsers = () => {
                                         <button
                                             type="button"
                                             className={`btn btn-circle btn-outline animate-pulse bg-info ${el?.role === 'admin' && 'animate-none'}`}
-                                            onClick={handeleInstrcutor}
-                                            disabled={isClicked || el?.role === 'admin'}>
+                                            onClick={() => handeleInstrcutor(el?._id)}
+                                            disabled={isClicked === el?.id || el?.role === 'admin' || el?.role === 'instructor'}>
                                             <MdEmojiPeople size={30} />
                                         </button>
                                     </td>
@@ -59,8 +106,8 @@ const ManageUsers = () => {
                                         <button
                                             type="button"
                                             className={`btn btn-circle btn-outline animate-pulse bg-success ${el?.role === 'admin' && 'animate-none'}`}
-                                            onClick={handeleAdmin}
-                                            disabled={isClicked || el?.role === 'admin'}>
+                                            onClick={() => handeleAdmin(el?._id)}
+                                            disabled={isClicked === el?._id || el?.role === 'admin' || el?.role === 'instructor'}>
                                             <RiAdminFill size={30} />
                                         </button>
                                     </td>

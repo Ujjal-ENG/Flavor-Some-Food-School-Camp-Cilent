143d3b640b50da419865c17fb9d8f414e0e634d9
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-07 17:34:15 +0600

    done the student routes

--- src/routes/StudentRoutes.jsx
+++ src/routes/StudentRoutes.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
+import useStudent from '../hooks/useStudent';
 
 const StudentRoutes = ({ children }) => {
     const { userInfo, privateLoad } = useAuth();
-    return <div>StudentRoutes</div>;
-};
+    const [isStudent, isStudentLoading] = useStudent();
+    const { pathname } = useLocation();
 
+    if (userInfo && isStudent) {
+        return children;
+    }
+    if (privateLoad || isStudentLoading) {
+        return (
+            <div className="h-screen flex justify-center items-center">
+                <progress className="progress w-56" />
+            </div>
+        );
+    }
+    return <Navigate to="/login" state={{ from: pathname }} />;
+};
 export default StudentRoutes;

9f693af215759f238950227234939563cc1f24b9
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-08 21:49:40 +0600

    payment integration done now teting not yet

--- src/components/pages/StudentPage/PaymentDetails.jsx
+++ src/components/pages/StudentPage/PaymentDetails.jsx
@@ -1,10 +1,29 @@
+/* eslint-disable comma-dangle */
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import SharedTitle from '../../layouts/shared/SharedTitle';
+import CheckoutForm from './CheckOutFrom';
+
+const ELEMENTS_OPTIONS = {
+    fonts: [
+        {
+            cssSrc: 'https://fonts.googleapis.com/css?family=Roboto'
+        }
+    ]
+};
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_SECRET_KEY);
 
 const PaymentDetails = () => {
     return (
         <div>
             <SharedTitle title1="Payment" title2="Details" />
+            <Elements stripe={stripePromise} options={ELEMENTS_OPTIONS}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };

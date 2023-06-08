acfe8ca79d9f23be5c7ecb855dc4355d796ecc09
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-08 20:30:40 +0600

    done the delete

--- src/components/pages/StudentPage/MySelectedClasses.jsx
+++ src/components/pages/StudentPage/MySelectedClasses.jsx
@@ -1,13 +1,29 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable operator-linebreak */
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable comma-dangle */
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import useAuth from '../../../hooks/useAuth';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import SharedTitle from '../../layouts/shared/SharedTitle';
 
 const MySelectedClasses = () => {
     const { userInfo, privateLoad } = useAuth();
+    const [loading, setIsLoading] = useState(false);
     const [axiosSecure] = useAxiosSecure();
-    const { data: selectedClass = [] } = useQuery({
+    const {
+        data: selectedClass = [],
+        isLoading,
+        refetch
+    } = useQuery({
         queryKey: ['selected-classes'],
         enabled: !!userInfo?.email && !privateLoad,
         queryFn: async () => {
@@ -15,10 +31,89 @@ const MySelectedClasses = () => {
             return data.data;
         }
     });
-    console.log(selectedClass);
+
+    const handleDeleteClasses = async (id) => {
+        setIsLoading(true);
+        try {
+            const result = await Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            });
+            if (result.isConfirmed) {
+                const { data } = await axiosSecure.delete(`/selected-classes/${userInfo?.email}`);
+                if (data) {
+                    Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+                    setIsLoading(false);
+                }
+            }
+        } catch (error) {
+            setIsLoading(false);
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: error.message
+            });
+        }
+    };
+
     return (
         <div>
             <SharedTitle title1="My" title2="Selected Classes" />
+            {isLoading && (
+                <div className="h-screen flex justify-center items-center">
+                    <progress className="progress w-56" />
+                </div>
+            )}
+            <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center gap-10 py-10">
+                {selectedClass.length <= 0 ? (
+                    <SharedTitle title1="Your Have not" title2="Selected any Classes" />
+                ) : (
+                    selectedClass.map((data) => (
+                        <div key={data._id} className={`overflow-hidden ${data?.availableSeats === 0 ? 'bg-red-500' : 'bg-white'} rounded shadow`}>
+                            <div className="p-5">
+                                <div className="relative">
+                                    <a href="#" title="" className="block ">
+                                        <img className="object-cover w-96 h-72" src={data?.image} alt={data?.name} />
+                                    </a>
+
+                                    <div className="absolute top-4 right-6">
+                                        <span className="px-4 py-2 text-2xl font-semibold tracking-widest text-white uppercase bg-black rounded-full">${data?.price} </span>
+                                    </div>
+                                </div>
+
+                                <h1 className="mt-5 text-3xl font-bold text-center">
+                                    ClassName: <span className="text-primary">{data?.name}</span>
+                                </h1>
+                                <div>
+                                    <h5 className="text-2xl font-bold py-5">
+                                        Instructor: <span className="text-primary">{data?.instructor}</span>
+                                    </h5>
+                                </div>
+                                {/* TODO: button will also disable when the role is isAdmin or isInstructors */}
+                                <div className="grid grid-cols-2 gap-5">
+                                    {loading ? (
+                                        <button className="btn btn-square">
+                                            <span className="loading loading-spinner" />
+                                        </button>
+                                    ) : (
+                                        <button type="button" className="btn btn-error font-bold  mt-5" onClick={() => handleDeleteClasses(data._id)}>
+                                            Delete
+                                        </button>
+                                    )}
+                                    <Link type="button" className="btn btn-primary font-bold  mt-5">
+                                        Payment
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                )}
+            </div>
         </div>
     );
 };

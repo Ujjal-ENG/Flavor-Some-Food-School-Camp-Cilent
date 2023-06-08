8b2ccfab4546de166fbbc1fbcc4369837c2237da
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-08 19:19:07 +0600

    My selected class done

--- src/components/pages/StudentPage/MySelectedClasses.jsx
+++ src/components/pages/StudentPage/MySelectedClasses.jsx
@@ -1,7 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import SharedTitle from '../../layouts/shared/SharedTitle';
 
 const MySelectedClasses = () => {
-    return <div>MySelectedClasses</div>;
+    const { userInfo, privateLoad } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+    const { data: selectedClass = [] } = useQuery({
+        queryKey: ['selected-classes'],
+        enabled: !!userInfo?.email && !privateLoad,
+        queryFn: async () => {
+            const { data } = await axiosSecure.get(`/selected-classes/${userInfo?.email}`);
+            return data.data;
+        }
+    });
+    console.log(selectedClass);
+    return (
+        <div>
+            <SharedTitle title1="My" title2="Selected Classes" />
+        </div>
+    );
 };
 
 export default MySelectedClasses;

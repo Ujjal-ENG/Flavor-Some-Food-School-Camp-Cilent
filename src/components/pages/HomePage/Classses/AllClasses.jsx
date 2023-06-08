6314f1477d2a7f0c51eecad6105af1a9325492a1
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-08 16:38:54 +0600

    added loading state

--- src/components/pages/HomePage/Classses/AllClasses.jsx
+++ src/components/pages/HomePage/Classses/AllClasses.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable max-len */
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
@@ -5,11 +6,15 @@ import ClassCard from './ClassCard';
 
 const AllClasses = () => {
     const [classes, setAllClasses] = useState([]);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
+        setLoading(true);
         axios.get('http://localhost:8080/classes').then(({ data }) => {
+            setLoading(false);
             return setAllClasses(data.data);
         });
     }, []);
+
     return (
         <div className="pt-24">
             <div className="flex-1 text-center lg:text-left">
@@ -18,7 +23,12 @@ const AllClasses = () => {
                     Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.
                 </p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center py-10 gap-10">{classes && classes.map((classItem) => <ClassCard key={classItem.id} data={classItem} />)}</div>
+            {loading && (
+                <div className="h-screen flex justify-center items-center">
+                    <progress className="progress w-56" />
+                </div>
+            )}
+            <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center py-10 gap-10">{classes && classes.map((classItem) => <ClassCard key={classItem._id} data={classItem} />)}</div>
         </div>
     );
 };

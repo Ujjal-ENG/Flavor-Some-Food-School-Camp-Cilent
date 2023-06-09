20af0b629284d210ecfaa30156320596d1e06a75
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-10 03:10:06 +0600

    some changes in desing related

--- src/components/pages/HomePage/Classses/AllClasses.jsx
+++ src/components/pages/HomePage/Classses/AllClasses.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable comma-dangle */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable max-len */
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import Typed from 'react-typed';
 import ClassCard from './ClassCard';
 
 const AllClasses = () => {
@@ -24,10 +27,19 @@ const AllClasses = () => {
             </Helmet>
             <div className="flex-1 text-center lg:text-left">
                 <h2 className="text-3xl font-bold pt-5 leading-tight text-black sm:text-4xl lg:text-5xl">All Classes from the all Instructors</h2>
-                <p className="max-w-2xl mx-auto mt-4 text-xl leading-relaxed text-gray-600 lg:mx-0">
-                    Discover a wide range of classes offered by various instructors. Whether you're looking to learn something new or enhance your skills, we have the perfect classes for you. Explore
-                    our diverse selection and embark on an exciting learning journey today!
-                </p>
+                <Typed
+                    className="text-[#4ADE80] text-center"
+                    strings={[
+                        "Discover a wide range of classes offered by various instructors. Whether you're looking to learn something new or enhance your skills, we have the perfect classes for you. Explore our diverse selection and embark on an exciting learning journey today!"
+                    ]}
+                    typeSpeed={30}
+                    style={{
+                        fontSize: '30px',
+                        fontWeight: 'bold',
+                        marginTop: '20px',
+                        lineHeight: '40px'
+                    }}
+                />
             </div>
             {loading && (
                 <div className="h-screen flex justify-center items-center">

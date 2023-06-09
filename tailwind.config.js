24c92bed3209de0ca852828010ca5e9f614225f9
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-09 19:16:59 +0600

    some eslint problem fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    darkMode: 'class',
     theme: {
         extend: {},
     },

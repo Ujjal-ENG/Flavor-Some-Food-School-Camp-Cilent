f29d1c4129e19017cb0e3d0bdd637a08ad901a6e
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-09 18:00:47 +0600

    some design related changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import Footer from './components/layouts/shared/Footer';
 import Navbar from './components/layouts/shared/Navbar';
 
@@ -9,6 +9,7 @@ const App = () => {
             <Navbar />
             <div className="max-w-screen-2xl mx-auto">
                 <Outlet />
+                <ScrollRestoration />
             </div>
             <Footer />
         </>

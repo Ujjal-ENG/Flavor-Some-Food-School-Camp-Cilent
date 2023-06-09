1fd209a8426db34d008821d63fd887366b994f4e
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-10 01:28:12 +0600

    Design some dashboard related worked

--- src/components/layouts/Dashboard/DashboardLayout.jsx
+++ src/components/layouts/Dashboard/DashboardLayout.jsx
@@ -1,11 +1,18 @@
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable comma-dangle */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { BiSelectMultiple } from 'react-icons/bi';
+import { HiClipboardCheck } from 'react-icons/hi';
+import { MdManageAccounts, MdOutlineManageAccounts } from 'react-icons/md';
 import { Link, Outlet } from 'react-router-dom';
+import Typed from 'react-typed';
 import useAdmin from '../../../hooks/useAdmin';
+import useAuth from '../../../hooks/useAuth';
 import useInstructor from '../../../hooks/useInstructor';
 import useStudent from '../../../hooks/useStudent';
 import Footer from '../shared/Footer';
@@ -17,6 +24,7 @@ const DashboardLayout = () => {
     const [isStudent] = useStudent();
     const [isAdmin] = useAdmin();
     const [isInstructor] = useInstructor();
+    const { userInfo } = useAuth();
 
     return (
         <>
@@ -35,18 +43,44 @@ const DashboardLayout = () => {
                     </div>
                     <div className="drawer-side">
                         <label htmlFor="my-drawer-2" className="drawer-overlay" />
-                        <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                        <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content space-y-4">
                             {/* Sidebar content here */}
                             {isStudent && (
                                 <>
-                                    {' '}
+                                    <Typed
+                                        className="text-primary text-center"
+                                        strings={['Student Dashboard']}
+                                        typeSpeed={80}
+                                        style={{
+                                            fontSize: '40px',
+                                            fontWeight: 'bold',
+                                            lineHeight: '70px',
+                                            marginBottom: '10px'
+                                        }}
+                                    />
+                                    <h1 className="text-center font-bold text-2xl">Hello</h1>
+                                    <Typed
+                                        className="text-[#4ADE80] text-center"
+                                        strings={[userInfo?.displayName]}
+                                        typeSpeed={80}
+                                        backSpeed={90}
+                                        style={{
+                                            fontSize: '40px',
+                                            fontWeight: 'bold',
+                                            lineHeight: '70px',
+                                            marginBottom: '10px'
+                                        }}
+                                        loop
+                                    />
                                     <li>
                                         <Link to="/dashboard/my-selected-classes" className="text-xl font-bold">
+                                            <BiSelectMultiple size={24} />
                                             My Selected Classes
                                         </Link>
                                     </li>
                                     <li>
                                         <Link to="/dashboard/my-enrolled-classes" className="text-xl font-bold">
+                                            <HiClipboardCheck size={24} />
                                             My Enrolled Classes
                                         </Link>
                                     </li>
@@ -54,14 +88,40 @@ const DashboardLayout = () => {
                             )}
                             {isAdmin && (
                                 <>
-                                    {' '}
+                                    <Typed
+                                        className="text-primary text-center"
+                                        strings={['Admin Dashboard']}
+                                        typeSpeed={80}
+                                        style={{
+                                            fontSize: '40px',
+                                            fontWeight: 'bold',
+                                            lineHeight: '70px',
+                                            marginBottom: '10px'
+                                        }}
+                                    />
+                                    <h1 className="text-center font-bold  text-2xl">Hello</h1>
+                                    <Typed
+                                        className="text-[#4ADE80] text-center"
+                                        strings={[userInfo?.displayName]}
+                                        typeSpeed={80}
+                                        backSpeed={90}
+                                        style={{
+                                            fontSize: '40px',
+                                            fontWeight: 'bold',
+                                            lineHeight: '70px',
+                                            marginBottom: '10px'
+                                        }}
+                                        loop
+                                    />
                                     <li>
                                         <Link to="/dashboard/manage-classes" className="text-xl font-bold">
+                                            <MdOutlineManageAccounts size={24} />
                                             Manage Classes
                                         </Link>
                                     </li>
                                     <li>
                                         <Link to="/dashboard/manage-users" className="text-xl font-bold">
+                                            <MdManageAccounts size={24} />
                                             Manage Users
                                         </Link>
                                     </li>
@@ -69,14 +129,41 @@ const DashboardLayout = () => {
                             )}
                             {isInstructor && (
                                 <>
-                                    {' '}
+                                    <Typed
+                                        className="text-primary text-center"
+                                        strings={['Instructor Dashboard']}
+                                        typeSpeed={80}
+                                        style={{
+                                            fontSize: '40px',
+                                            fontWeight: 'bold',
+                                            lineHeight: '70px',
+                                            marginBottom: '10px'
+                                        }}
+                                    />
+                                    <h1 className="text-center font-bold  text-2xl">Hello</h1>
+                                    <Typed
+                                        className="text-[#4ADE80] text-center"
+                                        strings={[userInfo?.displayName]}
+                                        typeSpeed={80}
+                                        backSpeed={90}
+                                        style={{
+                                            fontSize: '40px',
+                                            fontWeight: 'bold',
+                                            lineHeight: '70px',
+                                            marginBottom: '10px'
+                                        }}
+                                        loop
+                                    />
+
                                     <li>
                                         <Link to="/dashboard/add-a-class" className="text-xl font-bold">
+                                            <BiSelectMultiple size={24} />
                                             Add a Class
                                         </Link>
                                     </li>
                                     <li>
                                         <Link to="/dashboard/my-classes" className="text-xl font-bold">
+                                            <HiClipboardCheck size={24} />
                                             My Class
                                         </Link>
                                     </li>

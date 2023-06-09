97e9f93032596fb645913804eccad8b48f62a83a
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-09 16:17:03 +0600

    done the instrucotrs all page showing worked

--- src/components/pages/HomePage/Instructors/AllInstructors.jsx
+++ src/components/pages/HomePage/Instructors/AllInstructors.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+/* eslint-disable max-len */
+/* eslint-disable no-underscore-dangle */
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import SharedTitle from '../../../layouts/shared/SharedTitle';
+import InstructorCard from './InstructorCard';
 
 const AllInstructors = () => {
-    return <div>AllInstructors</div>;
+    const [allInstructors, setAllInstructors] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:8080/all-instructors').then(({ data }) => setAllInstructors(data.data));
+    }, []);
+    console.log(allInstructors);
+    return (
+        <div className="pt-20">
+            <SharedTitle title1="All  " title2="Instructors" />
+            <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center gap-10 py-10">{allInstructors && allInstructors.map((el) => <InstructorCard key={el._id} data={el} />)}</div>
+        </div>
+    );
 };
 
 export default AllInstructors;
---
+++ src/components/pages/HomePage/Instructors/InstructorCard.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable comma-dangle */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { motion } from 'framer-motion';
+import React from 'react';
+
+const InstructorCard = ({ data }) => {
+    return (
+        <motion.div
+            initial={{ y: 1000, opacity: 0, scale: 0.5 }}
+            animate={{ y: 0, opacity: 1, scale: 1 }}
+            transition={{
+                duration: 1.5,
+                ease: [0, 0.71, 0.2, 1.01],
+                scale: {
+                    type: 'spring',
+                    damping: 25,
+                    stiffness: 100,
+                    restDelta: 0.001
+                }
+            }}>
+            <div className="overflow-hidden bg-white rounded shadow-md duration-200 transition-all ease-in-out hover:shadow-2xl">
+                <div className="p-5">
+                    <div className="relative">
+                        <a href="#" title="" className="block ">
+                            <img className="object-cover w-96 h-72" src={data?.image} alt={data?.name} />
+                        </a>
+                    </div>
+
+                    <h1 className="mt-5 text-3xl font-bold text-center">{data?.name}</h1>
+                    <h1 className="mt-5 text-xl font-bold text-primary text-center">{data?.email}</h1>
+                </div>
+            </div>
+        </motion.div>
+    );
+};
+
+export default InstructorCard;

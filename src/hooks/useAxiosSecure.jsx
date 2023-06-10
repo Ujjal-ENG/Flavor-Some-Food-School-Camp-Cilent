687c17d244cb49a215b1982391128bdbddafedff
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-11 00:40:20 +0600

    one routes problem solved

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://flavorsome-food-school-server.vercel.app'
 });
 
 const useAxiosSecure = () => {

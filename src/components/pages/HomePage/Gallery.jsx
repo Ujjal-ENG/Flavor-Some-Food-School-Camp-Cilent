445d06d4ef90b6e0e224866aa662e4563296eb28
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-18 15:30:30 +0600

    added the social media icons

--- src/components/pages/HomePage/Gallery.jsx
+++ src/components/pages/HomePage/Gallery.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable max-len */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/jsx-indent-props */
@@ -30,6 +31,30 @@ const Gallery = () => {
                                             <p className="text-xl tracking-wider text-white">
                                                 Master the art of baking as campers create delectable desserts, pastries, bread, and other sweet treats under the guidance of our skilled pastry chefs.
                                             </p>
+                                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                                                <a className="text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                                                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
+                                                        <circle cx="4" cy="4" r="2" stroke="none" />
+                                                    </svg>
+                                                </a>
+                                            </span>
                                         </div>
                                     </div>
                                 </div>
@@ -48,6 +73,30 @@ const Gallery = () => {
                                                 We maintain a clean and well-equipped kitchen facility, ensuring that all participants adhere to proper food handling practices and sanitation
                                                 guidelines.
                                             </p>
+                                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                                                <a className="text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                                                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
+                                                        <circle cx="4" cy="4" r="2" stroke="none" />
+                                                    </svg>
+                                                </a>
+                                            </span>
                                         </div>
                                     </div>
                                 </div>
@@ -67,6 +116,30 @@ const Gallery = () => {
                                                 their peers, work in groups to prepare meals, and present their culinary creations. They will also participate in fun challenges, friendly competitions,
                                                 and food-themed games, fostering a sense of camaraderie and fostering a love for the culinary arts.
                                             </p>
+                                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                                                <a className="text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                                                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
+                                                        <circle cx="4" cy="4" r="2" stroke="none" />
+                                                    </svg>
+                                                </a>
+                                            </span>
                                         </div>
                                     </div>
                                 </div>
@@ -90,6 +163,30 @@ const Gallery = () => {
                                                 preparation techniques. Our experienced chefs and culinary experts will guide campers through hands-on cooking lessons, interactive workshops, and
                                                 engaging food-related activities.
                                             </p>
+                                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                                                <a className="text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                                                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
+                                                        <circle cx="4" cy="4" r="2" stroke="none" />
+                                                    </svg>
+                                                </a>
+                                            </span>
                                         </div>
                                     </div>
                                 </div>
@@ -108,6 +205,30 @@ const Gallery = () => {
                                             <p className="text-xl tracking-wider text-white">
                                                 Learn about the importance of fresh, locally-sourced ingredients and how to create delicious, healthy meals using seasonal produce.
                                             </p>
+                                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                                                <a className="text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                                                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
+                                                        <circle cx="4" cy="4" r="2" stroke="none" />
+                                                    </svg>
+                                                </a>
+                                            </span>
                                         </div>
                                     </div>
                                 </div>
@@ -125,6 +246,30 @@ const Gallery = () => {
                                             <p className="text-xl tracking-wider text-white">
                                                 Dive into the science behind cooking, understanding the nutritional value of ingredients, and exploring the impact of food on our health.
                                             </p>
+                                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                                                <a className="text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                                                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                                                    </svg>
+                                                </a>
+                                                <a className="ml-3 text-white text-3xl py-3">
+                                                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+                                                        <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
+                                                        <circle cx="4" cy="4" r="2" stroke="none" />
+                                                    </svg>
+                                                </a>
+                                            </span>
                                         </div>
                                     </div>
                                 </div>

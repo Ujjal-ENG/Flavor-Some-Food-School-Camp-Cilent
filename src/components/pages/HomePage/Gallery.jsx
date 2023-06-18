68c7f287db8c9407d7363d42fce02ad19f6ea3ee
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-18 15:26:42 +0600

    done the gallery section

--- src/components/pages/HomePage/Gallery.jsx
+++ src/components/pages/HomePage/Gallery.jsx
@@ -18,48 +18,116 @@ const Gallery = () => {
                     <div className="flex flex-wrap md:-m-2 -m-1">
                         <div data-aos="fade-up" className="flex flex-wrap w-1/2">
                             <div className="md:p-2 p-1 w-1/2">
-                                <img
-                                    alt="gallery"
-                                    className="w-full object-cover h-full object-center block"
-                                    src="https://images.pexels.com/photos/6743248/pexels-photo-6743248.jpeg?auto=compress&cs=tinysrgb&w=600https://images.pexels.com/photos/6743248/pexels-photo-6743248.jpeg?auto=compress&cs=tinysrgb&w=600"
-                                />
+                                <div className="group relative w-full h-full">
+                                    <img
+                                        alt="gallery"
+                                        className="w-full object-cover h-full object-center block"
+                                        src="https://images.pexels.com/photos/6743248/pexels-photo-6743248.jpeg?auto=compress&cs=tinysrgb&w=600https://images.pexels.com/photos/6743248/pexels-photo-6743248.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                    />
+                                    <div className="absolute inset-0 bg-gray-900 bg-opacity-0 group-hover:bg-opacity-70 transition-opacity duration-300 flex items-center justify-center opacity-0 group-hover:opacity-100">
+                                        <div className="px-10 text-center">
+                                            <h3 className="text-3xl font-bold text-white mb-2">Baking and Pastry</h3>
+                                            <p className="text-xl tracking-wider text-white">
+                                                Master the art of baking as campers create delectable desserts, pastries, bread, and other sweet treats under the guidance of our skilled pastry chefs.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             <div className="md:p-2 p-1 w-1/2">
-                                <img
-                                    alt="gallery"
-                                    className="w-full object-cover h-full object-center block"
-                                    src="https://images.pexels.com/photos/6758634/pexels-photo-6758634.jpeg?auto=compress&cs=tinysrgb&w=600"
-                                />
+                                <div className="group relative w-full h-full">
+                                    <img
+                                        alt="gallery"
+                                        className="w-full object-cover h-full object-center block"
+                                        src="https://images.pexels.com/photos/6758634/pexels-photo-6758634.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                    />
+                                    <div className="absolute inset-0 bg-gray-900 bg-opacity-0 group-hover:bg-opacity-70 transition-opacity duration-300 flex items-center justify-center opacity-0 group-hover:opacity-100">
+                                        <div className="px-10 text-center">
+                                            <h3 className="text-3xl font-bold text-white mb-2">Safety and hygiene are our utmost priority.</h3>
+                                            <p className="text-xl tracking-wider text-white">
+                                                We maintain a clean and well-equipped kitchen facility, ensuring that all participants adhere to proper food handling practices and sanitation
+                                                guidelines.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             <div className="md:p-2 p-1 w-full">
-                                <img
-                                    alt="gallery"
-                                    className="w-full object-cover h-full object-center block"
-                                    src="https://images.pexels.com/photos/8033812/pexels-photo-8033812.jpeg?auto=compress&cs=tinysrgb&w=600"
-                                />
+                                <div className="group relative w-full h-full">
+                                    <img
+                                        alt="gallery"
+                                        className="w-full object-cover h-full object-center block"
+                                        src="https://images.pexels.com/photos/8033812/pexels-photo-8033812.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                    />
+                                    <div className="absolute inset-0 bg-gray-900 bg-opacity-0 group-hover:bg-opacity-70 transition-opacity duration-300 flex items-center justify-center opacity-0 group-hover:opacity-100">
+                                        <div className="px-10 text-center">
+                                            <h3 className="text-3xl font-bold text-white mb-2">Team work Experiences</h3>
+                                            <p className="text-xl tracking-wider text-white">
+                                                Our Summer Food Camp not only focuses on cooking skills but also emphasizes teamwork, creativity, and problem-solving. Campers will collaborate with
+                                                their peers, work in groups to prepare meals, and present their culinary creations. They will also participate in fun challenges, friendly competitions,
+                                                and food-themed games, fostering a sense of camaraderie and fostering a love for the culinary arts.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+
                         <div data-aos="fade-down" className="flex flex-wrap w-1/2">
                             <div className="md:p-2 p-1 w-full">
-                                <img
-                                    alt="gallery"
-                                    className="w-full h-full object-cover object-center block"
-                                    src="https://images.pexels.com/photos/8035107/pexels-photo-8035107.jpeg?auto=compress&cs=tinysrgb&w=600"
-                                />
+                                <div className="group relative w-full h-full hover:shadow-xl">
+                                    <img
+                                        alt="gallery"
+                                        className="w-full h-full object-cover object-center block"
+                                        src="https://images.pexels.com/photos/8035107/pexels-photo-8035107.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                    />
+                                    <div className="absolute inset-0 bg-gray-900 bg-opacity-0 group-hover:bg-opacity-70 transition-opacity duration-300 flex items-center justify-center opacity-0 group-hover:opacity-100">
+                                        <div className="flex justify-center items-center flex-col px-20">
+                                            <h3 className="text-3xl font-bold text-white mb-2">Global Gastronomy</h3>
+                                            <p className="text-xl tracking-wider text-white">
+                                                Delve into the diverse cuisines of various countries, such as Italian pasta making, Mexican taco fiestas, Japanese sushi rolling, and more. During the
+                                                camp, participants will have the opportunity to sharpen their culinary skills, discover new ingredients, and develop a deeper understanding of food
+                                                preparation techniques. Our experienced chefs and culinary experts will guide campers through hands-on cooking lessons, interactive workshops, and
+                                                engaging food-related activities.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="md:p-2 p-1 w-1/2">
+                                <div className="group relative w-full h-full">
+                                    <img
+                                        alt="gallery"
+                                        className="w-full object-cover h-full object-center block"
+                                        src="https://images.pexels.com/photos/6482320/pexels-photo-6482320.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                    />
+                                    <div className="absolute inset-0 bg-gray-900 bg-opacity-0 group-hover:bg-opacity-70 transition-opacity duration-300 flex items-center justify-center opacity-0 group-hover:opacity-100">
+                                        <div className="px-10">
+                                            <h3 className="text-3xl font-bold text-white mb-2">Farm-to-Table Experience</h3>
+                                            <p className="text-xl tracking-wider text-white">
+                                                Learn about the importance of fresh, locally-sourced ingredients and how to create delicious, healthy meals using seasonal produce.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             <div className="md:p-2 p-1 w-1/2">
-                                <img
-                                    alt="gallery"
-                                    className="w-full object-cover h-full object-center block"
-                                    src="https://images.pexels.com/photos/6482320/pexels-photo-6482320.jpeg?auto=compress&cs=tinysrgb&w=600"
-                                />
-                            </div>
-                            <div className="md:p-2 p-1 w-1/2">
-                                <img
-                                    alt="gallery"
-                                    className="w-full object-cover h-full object-center block"
-                                    src="https://images.pexels.com/photos/8082787/pexels-photo-8082787.jpeg?auto=compress&cs=tinysrgb&w=600"
-                                />
+                                <div className="group relative w-full h-full">
+                                    <img
+                                        alt="gallery"
+                                        className="w-full object-cover h-full object-center block"
+                                        src="https://images.pexels.com/photos/8082787/pexels-photo-8082787.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                    />
+                                    <div className="absolute inset-0 bg-gray-900 bg-opacity-0 group-hover:bg-opacity-70 transition-opacity duration-300 flex items-center justify-center opacity-0 group-hover:opacity-100">
+                                        <div className="px-10">
+                                            <h3 className="text-3xl font-bold text-white mb-2">Food Science and Nutrition</h3>
+                                            <p className="text-xl tracking-wider text-white">
+                                                Dive into the science behind cooking, understanding the nutritional value of ingredients, and exploring the impact of food on our health.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
